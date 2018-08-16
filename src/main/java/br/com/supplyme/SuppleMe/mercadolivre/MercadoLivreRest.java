b8b86106a2f4e9b9d2ed214b7f2827f4303dcb9c
Hussein <hussein.ali@becommerce.com.br>
2018-08-16 09:26:19 -0300

    Fix

--- src/main/java/br/com/supplyme/SuppleMe/mercadolivre/MercadoLivreRest.java
+++ src/main/java/br/com/supplyme/SuppleMe/mercadolivre/MercadoLivreRest.java
@@ -1,15 +1,12 @@
 package br.com.supplyme.SuppleMe.mercadolivre;
 
-import javax.ws.rs.PathParam;
-import javax.ws.rs.QueryParam;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @CrossOrigin(origins = "*")
@@ -24,7 +21,7 @@ public class MercadoLivreRest {
 
 	
 	@GetMapping(path = "/redirect")
-	public String redirectMeli(@QueryParam("code") String subscriptionCode) {
+	public String redirectMeli(@RequestParam("code") String subscriptionCode) {
 		LOGGER.info("Redirect works!");
 		
 		this.mercadoLivreService.cadastrarProduto(subscriptionCode);
