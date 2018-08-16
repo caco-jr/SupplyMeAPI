f6984f647ad898d869a38298a068c62a343b5980
Hussein <hussein.ali@becommerce.com.br>
2018-08-16 04:37:50 -0300

    afasd

--- src/main/java/br/com/supplyme/SuppleMe/model/Usuario.java
+++ src/main/java/br/com/supplyme/SuppleMe/model/Usuario.java
@@ -10,79 +10,79 @@ import javax.persistence.Id;
 @Entity
 public class Usuario {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
-	private String nome;
-	private String sobrenome;
-	private String documento;
-	private Date dataNascimento;
-	private String email;
-	private String senha;
-	private String tipoUsuario;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String sobrenome;
+    private String documento;
+    private Date dataNascimento;
+    private String email;
+    private String senha;
+    private String tipoUsuario;
 
-	public Long getId() {
-		return id;
-	}
+    public Long getId() {
+        return id;
+    }
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-	public String getNome() {
-		return nome;
-	}
+    public String getNome() {
+        return nome;
+    }
 
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 
-	public String getSobrenome() {
-		return sobrenome;
-	}
+    public String getSobrenome() {
+        return sobrenome;
+    }
 
-	public void setSobrenome(String sobrenome) {
-		this.sobrenome = sobrenome;
-	}
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
 
-	public String getDocumento() {
-		return documento;
-	}
+    public String getDocumento() {
+        return documento;
+    }
 
-	public void setDocumento(String documento) {
-		this.documento = documento;
-	}
+    public void setDocumento(String documento) {
+        this.documento = documento;
+    }
 
-	public Date getDataNascimento() {
-		return dataNascimento;
-	}
+    public Date getDataNascimento() {
+        return dataNascimento;
+    }
 
-	public void setDataNascimento(Date dataNascimento) {
-		this.dataNascimento = dataNascimento;
-	}
+    public void setDataNascimento(Date dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
 
-	public String getEmail() {
-		return email;
-	}
+    public String getEmail() {
+        return email;
+    }
 
-	public void setEmail(String email) {
-		this.email = email;
-	}
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
-	public String getSenha() {
-		return senha;
-	}
+    public String getSenha() {
+        return senha;
+    }
 
-	public void setSenha(String senha) {
-		this.senha = senha;
-	}
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
 
-	public String getTipoUsuario() {
-		return tipoUsuario;
-	}
+    public String getTipoUsuario() {
+        return tipoUsuario;
+    }
 
-	public void setTipoUsuario(String tipoUsuario) {
-		this.tipoUsuario = tipoUsuario;
-	}
+    public void setTipoUsuario(String tipoUsuario) {
+        this.tipoUsuario = tipoUsuario;
+    }
 
 }
