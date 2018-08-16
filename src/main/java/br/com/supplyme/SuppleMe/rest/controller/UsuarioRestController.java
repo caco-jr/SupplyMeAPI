98e47d82c3623a1caedf2083cf472ea50633e7c4
Hussein <hussein.ali@becommerce.com.br>
2018-08-16 00:11:09 -0300

    Feat: Hello World

--- src/main/java/br/com/supplyme/SuppleMe/rest/controller/UsuarioRestController.java
+++ src/main/java/br/com/supplyme/SuppleMe/rest/controller/UsuarioRestController.java
@@ -1,5 +1,19 @@
 package br.com.supplyme.SuppleMe.rest.controller;
 
-public class UsuarioRestController {
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin( origins = "*" )
+@RestController
+@RequestMapping( value = "/usuario")
+public class UsuarioRestController {
+	
+	
+	@GetMapping("test")
+	public String helloWorld() {
+		
+		return "Hello World";
+	}
 }
