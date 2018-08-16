f3270d5f77059d2966dea8d3e9fa6528be5bd147
Hussein <hussein.ali@becommerce.com.br>
2018-08-15 22:28:48 -0300

    First commit

---
+++ src/main/java/br/com/supplyme/SuppleMe/SuppleMeApplication.java
@@ -0,0 +1,12 @@
+package br.com.supplyme.SuppleMe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuppleMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuppleMeApplication.class, args);
+	}
+}
---
+++ src/test/java/br/com/supplyme/SuppleMe/SuppleMeApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.supplyme.SuppleMe;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SuppleMeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
